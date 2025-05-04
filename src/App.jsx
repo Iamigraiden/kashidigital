33e7d8482d7d826d7b935e11fe7e65ad976889f2
Abishek Thakur <abishekthakur8862@gmail.com>
2025-05-04 18:11:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from './components/common/Header'
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import './../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from './pages/Home.jsx';
 import Footer from './components/common/Footer.jsx';
