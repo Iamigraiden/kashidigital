3f2eb79f63e9ec98fad9766e261b695baa1cd5d5
Abishek Thakur <abishekthakur8862@gmail.com>
2025-05-14 16:09:41 +0530

    jnsj

--- src/components/forms/Popup.jsx
+++ src/components/forms/Popup.jsx
@@ -4,8 +4,6 @@ import React from 'react';
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import axios from 'axios';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const initialValues = {
   name: "",
@@ -35,7 +33,7 @@ const Popup = () => {
         );
 
         if (response.status === 200) {
-          toast.success('Request submitted successfully!');
+          alert('Request submitted successfully!');
           resetForm();
         } 
         else {
